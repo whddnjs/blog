99bf85d125f5a49ac44ba86a6b7bdaee4a2aa296
jongwon <kjw_0117@naver.com>
2022-07-27 23:59:36 +0900

    0727db연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,41 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const SERVER_URL = 'http://localhost:4000/write';
+const SERVER_URL = 'http://localhost:4000';
 function App() {
   const [modal, setModal] = useState(false);
   const [notice, setNotice] = useState(null);
 
   const fetchData = async () => {
     const res = await axios.get(SERVER_URL);
-    console.log(res.data);
-    setNotice(res.data);
+    console.log(res.data.posts);
+    setNotice(res.data.posts);
   };
+
+  // const addData = async ()=>{
+  //   const res = await axios.get()
+  // }
+
   useEffect(() => {
     fetchData();
-  }, [notice]);
+  }, []);
 
-  const obSubmitHandler = e => {
+  const obSubmitHandler = async e => {
     e.preventDefault();
     const title = e.target.title.value;
     const content = e.target.content.value;
     const writer = e.target.writer.value;
-    axios.post(SERVER_URL, { title, content, writer });
+    await axios.post('http://localhost:4000/add', { title, content, writer });
+    console.log('zz');
     fetchData();
     setModal(false);
+    console.log(notice);
   };
 
   return (
     <div className="w-full h-screen">
       <button
-        className="border border-black p-1"
+        className="p-1 border border-black"
         onClick={() => {
           setModal(true);
         }}
@@ -40,8 +47,10 @@ function App() {
         <ul>
           {notice?.map(item => (
             <li className="flex" key={item.id}>
+              <div className="mr-20">{item._id}</div>
               <div className="mr-20">{item.title}</div>
               <div>{item.writer}</div>
+              <button className="ml-2 border">삭제</button>
             </li>
           ))}
         </ul>
@@ -51,20 +60,20 @@ function App() {
         <div className="w-[500px] h-[600px] border-2 absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-3">
           <form onSubmit={obSubmitHandler}>
             <p>제목</p>
-            <input type="text" className="border p-2 w-full" name="title" />
+            <input type="text" className="w-full p-2 border" name="title" />
             <p>내용</p>
             <input
               type="text"
-              className="border p-2 w-full h-80"
+              className="w-full p-2 border h-80"
               name="content"
             />
             <p>작성자</p>
-            <input type="text" className="border p-2" name="writer" />
+            <input type="text" className="p-2 border" name="writer" />
             <div className="flex justify-center mt-16">
-              {/* <button className="mr-4 border p-1">작성</button> */}
-              <input type="submit" className="mr-4 border p-1" value="작성" />
+              {/* <button className="p-1 mr-4 border">작성</button> */}
+              <input type="submit" className="p-1 mr-4 border" value="작성" />
               <button
-                className="border p-1"
+                className="p-1 border"
                 onClick={() => {
                   setModal(false);
                 }}
--- server/app.js
+++ server/app.js
@@ -1,26 +1,74 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-
+const MongoClient = require('mongodb').MongoClient;
 app.use(cors());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+require('dotenv').config();
 
-let id = 1;
-const notice = [
-  {
-    id: 0,
-    title: '제목0',
-    content: '내용0',
-    writer: '작성자0',
-  },
-];
+let db;
+MongoClient.connect(process.env.DB_URL, (err, client) => {
+  // 에러표시
+  if (err) return console.log(err);
 
-app.get('/write', (req, res) => {
-  res.json(notice);
+  db = client.db('blog');
+
+  app.listen(process.env.PORT, () => {
+    console.log('port 4000 on');
+  });
 });
 
-app.post('/write', (req, res) => {
+// let id = 1;
+// const notice = [
+//   {
+//     id: 0,
+//     title: '제목0',
+//     content: '내용0',
+//     writer: '작성자0',
+//   },
+// ];
+
+// app.get('/', (req, res) => {
+//   res.json(notice);
+// });
+
+app.get('/', (req, res) => {
+  db.collection('notice')
+    .find()
+    .toArray((err, rst) => {
+      console.log(rst);
+      res.json({ posts: rst });
+    });
+});
+
+app.post('/add', (req, res) => {
+  console.log(req.body);
+  db.collection('id').findOne({ name: 'id' }, (err, rst) => {
+    console.log(rst.totalPost);
+
+    const 저장할거 = {
+      _id: rst.totalPost,
+      title: req.body.title,
+      content: req.body.content,
+      writer: req.body.writer,
+    };
+
+    db.collection('notice').insertOne(저장할거, (err, rst) => {
+      console.log('저장완료');
+      db.collection('id').updateOne(
+        { name: 'id' },
+        { $inc: { totalPost: 1 } },
+        (err, rst) => {
+          if (err) return console.log(err);
+        }
+      );
+    });
+  });
+  return res.send('success');
+});
+
+app.post('/', (req, res) => {
   console.log(req.body);
   const { title, content, writer } = req.body;
   notice.push({
@@ -29,8 +77,5 @@ app.post('/write', (req, res) => {
     content,
     writer,
   });
-});
-
-app.listen(4000, () => {
-  console.log('server start');
+  res.send('seccess');
 });
